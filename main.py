ca438d2a629a1012409092b8db62b7bd9541529b
KasperDGit <163639121+KasperDGit@users.noreply.github.com>
2026-03-18 00:31:34 +0500

    Add LED control and fix print statement

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 # This is a test file of the OTA code
+from machine import Pin
+import time
 
 print('Hello Worl')
+
+# Setup LED on Pin 8
+led = Pin(8, Pin.OUT)
+
+while True:
+    led.value(1)    # Turn LED on
+    time.sleep(0.5) # Sleep for 0.5 seconds
+    led.value(0)    # Turn LED off
+    time.sleep(0.5) # Sleep for 0.5 seconds
