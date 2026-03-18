9dceabd8ca319c3b55077747d547e8d965629457
KasperDGit <163639121+KasperDGit@users.noreply.github.com>
2026-03-18 14:46:07 +0500

    Integrate test module and update LED control logic
    Added a new test module and modified the main loop to include test functions.

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
-# This is a test file of the OTA code
 from machine import Pin
 import time
+import test   # 👈 import your new file
 
 print('Hello from Git')
 
 # Setup LED on Pin 8
 led = Pin(8, Pin.OUT)
 
+# Call function from test.py
+test.run_test()
+
 while True:
-    led.value(1)    # Turn LED on
-    time.sleep(1) # Sleep for 1 seconds
-    led.value(0)    # Turn LED off
-    time.sleep(1) # Sleep for 1 seconds
+    for i in range(5):
+        led.value(0)
+        time.sleep(1)
+        led.value(1)
+        time.sleep(1)
+
+    # Optional: call test blink
+    test.blink_fast(led)
+    
+def run_test():
+    print("Test.py is working!")
+
+def blink_fast(led):
+    import time
+    for i in range(5):
+        led.value(0)
+        time.sleep(0.2)
+        led.value(1)
+        time.sleep(0.2)
